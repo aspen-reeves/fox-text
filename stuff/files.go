fb31c946a1d5fc9e43222d044e8f67f3ab1ed18b
aspen-reeves <aspenbreeves@gmail.com>
2022-10-11 16:33:36 -0500

    no

--- stuff/files.go
+++ stuff/files.go
@@ -25,7 +25,7 @@ func SaveFile(lines []string) error {
 		err := errors.New("please provide a file name")
 		panic(err)
 	}
-	err := os.WriteFile(os.Args[1], data, 0644)
+	err := os.WriteFile(os.Args[1], data, 0644) //write to file
 	if err != nil {
 		err = errors.New("cant write file")
 		panic(err)
@@ -35,8 +35,8 @@ func SaveFile(lines []string) error {
 func ByteToStr(data []byte) []string {
 	//convert file to array of strings
 	lines := make([]string, 0)
-	for _, line := range bytes.Split(data, []byte{'\n'}) {
-		lines = append(lines, string(line))
+	for _, line := range bytes.Split(data, []byte{'\n'}) { //split data by newline
+		lines = append(lines, string(line)) // add line to lines
 	}
 
 	return lines
@@ -44,11 +44,11 @@ func ByteToStr(data []byte) []string {
 }
 func StrToByte(lines []string) []byte {
 	//convert array of strings to byte array
-	data := make([]byte, 0)
-	for i := 0; i < len(lines); i++ {
-		data = append(data, []byte(lines[i])...)
-		data = append(data, '\n')
+	data := make([]byte, 0)           //create byte array
+	for i := 0; i < len(lines); i++ { //loop through lines
+		data = append(data, []byte(lines[i])...) //add line to data
+		data = append(data, '\n')                //add newline to data
 	}
-	return data
+	return data //return data
 
 }
